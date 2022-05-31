de563f1ad7b59143669af04776f4d47aa660377b
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2022-05-31 12:56:29 +0900

    UI追加

---
+++ Assets/Sc/Score.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField, Tooltip("トータルスコア")] int _addScore = 0;
+
+     public void ScoreButtom()
+    {
+        _addScore++;
+    }
+}
