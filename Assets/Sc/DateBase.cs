22e4bdda9cc964e81a40a66216bd821c05b09d18
DESKTOP-K84D808\U5-11 <yu.love5416@gmail.com>
2022-06-20 15:53:45 +0900

    図鑑関連スタート

---
+++ Assets/Sc/DateBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DateBase : MonoBehaviour
+{
+    [SerializeField] List<GameObject> _mashPhoto = new List<GameObject>();
+    [SerializeField] List<GameObject> _mashMoji = new List<GameObject>();
+    [SerializeField] List<bool> IsYouWin = new List<bool>();
+
+    [SerializeField] GameObject _missingPhoto = default;
+
+    [SerializeField] GameObject _photoPos = default;
+    [SerializeField] GameObject _mojiPos = default;
+
+    List<string> _sortMash = new List<string>();
+    void Start()
+    {
+    }
+    public void OnNextClick()
+    {
+
+    }
+    public void OnReturnClick()
+    {
+
+    }
+
+    void ListSort()
+    {
+        for (int i = 0; i < _mashPhoto.Count; i++)
+        {
+            _sortMash[i] = _mashPhoto[i].name;
+        }
+        _sortMash.Sort();
+    }
+}
---
+++ Assets/Sc/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortClass : MonoBehaviour
+{
+    public string Name;
+    public GameObject GameObject;
+
+    public SortClass(string Name, GameObject GameObject)
+    {
+        this.Name = Name;
+        this.GameObject = GameObject;
+    }
+}
